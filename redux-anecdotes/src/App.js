8c04f298297fbe756dba8388e7d9dcfde377e8a1
Heikki Koivula <heikki.koivula@gmail.com>
2022-01-25 06:51:36 +0200

    exercise 6.7 complete
    make adding an anecdote an own component

--- redux-anecdotes/src/App.js
+++ redux-anecdotes/src/App.js
@@ -4,18 +4,11 @@ import {
   voteAnecdote,
   createAnecdote
 } from './reducers/anecdoteReducer'
+import NewAnecdote from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector((state) => state)
   const dispatch = useDispatch()
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    console.log(event.target.anecdote.value)
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-  }
 
   const vote = (id) => {
     console.log('vote', id)
@@ -36,11 +29,7 @@ const App = () => {
             </div>
           </div>
         ))}
-      <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <input name="anecdote" />
-        <button type="submit">create</button>
-      </form>
+      <NewAnecdote />
     </div>
   )
 }
